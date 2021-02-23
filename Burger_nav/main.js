0b7292c72136f0290a63b8ca0e044b337768d45f
MilosStankovic93 <79517422+MilosStankovic93@users.noreply.github.com>
2021-02-23 13:23:40 +0100

    Add files via upload

---
+++ Burger_nav/main.js
@@ -0,0 +1,12 @@
+var menuBtn = document.querySelector('.menu-btn');
+var menuOpen = false;
+menuBtn.addEventListener('click', () =>{
+	if(!menuOpen){
+		menuBtn.classList.add('open');
+		menuOpen = true;
+	}
+	else{
+		menuBtn.classList.remove('open')
+		menuOpen = false;
+	}
+})
